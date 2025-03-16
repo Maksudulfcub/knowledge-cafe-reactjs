f47789b351a43e96ca3112baf6988671df1b0981
Maksudul <maaksuduldish@gmail.com>
2025-03-16 11:55:04 +0600

    Files setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <div className='max-w-6xl border mx-auto'>
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1 className="font-bold text-center">Navbar title</h1>
+        </div>
+    );
+};
+
+export default Navbar;
